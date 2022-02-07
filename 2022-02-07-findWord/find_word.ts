db71ec9238956409e3e1ec839c6e537d3c14dc84
Will Fitzgerald <willf@users.noreply.github.com>
2022-02-07 13:57:54 -0500

    add test case and new problem description

--- 2022-02-07-findWord/find_word.ts
+++ 2022-02-07-findWord/find_word.ts
@@ -14,13 +14,31 @@ $ [['*','a','q','t'],
    
   */
 
-function replaceLetter(letter: String, line: Array<String>):Array<String> {
-  return line.map(l => l === letter ? '*' : l)
+function replaceFirstOccurrence(letter: String, line: Array<String>): Array<String> {
+  let index = line.indexOf(letter);
+  if (index > -1) {
+    return line.map((l, i) => i === index ? '*' : l)
+  }
+  return line
 }
 
 function findWord(grid: Array<Array<String>>, word: String): Array<Array<String>> {
-  return grid.map((line, index) => replaceLetter(word[index], line))
+  return grid.map((line, index) => replaceFirstOccurrence(word[index], line))
 }
 
 
-// var results = findWord([['a','a','q','t'],['x','c','w','e'],['r','l','e','p']], 'ace')
+// Test
+let grid = [['a', 'a', 'q', 't'],
+  ['x', 'c', 'w', 'e'],
+  ['r', 'l', 'e', 'p']]
+
+let word = 'ace'
+
+let target = [['*', 'a', 'q', 't'],
+  ['x', '*', 'w', 'e'],
+  ['r', 'l', '*', 'p']]
+
+let actual = findWord(grid, word)
+
+let truth = actual.every((line, i) => line.every((letter, j) => letter === target[i][j]))
+console.assert(truth, 'The arrays were not equal')
