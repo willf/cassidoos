0679eabc1dbf43c21f763e24f056b11b5825c256
Will Fitzgerald <willf@github.com>
2022-04-18 12:15:48 -0400

    cache out subarrays first (unfortunately)

--- 2022-04-18-subarray-of-length-n-with-highest-sum/largest-subarray-by-sum.py
+++ 2022-04-18-subarray-of-length-n-with-highest-sum/largest-subarray-by-sum.py
@@ -18,10 +18,10 @@ def largestSubarraySum(arr, n):
     >>> largestSubarraySum([3,1,4,1,5,9,2,6], 0)
     []
     """
-    if n > len(arr) or n <= 0:
+    subarrays = list(zip(*[arr[i:] for i in range(n)]))
+    if not subarrays:
         return []
-    subarrays = zip(*[arr[i:] for i in range(n)])
-    return list(max(subarrays, key=sum))
+    return max(subarrays, key=sum)  
 
 if __name__ == '__main__':
     import doctest
