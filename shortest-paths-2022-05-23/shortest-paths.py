040b5bbee3ee7724ea0415db8b8ae84b7babb28f
Will Fitzgerald <willf@github.com>
2022-05-23 10:14:55 -0400

    shortest-paths.py

---
+++ shortest-paths-2022-05-23/shortest-paths.py
@@ -0,0 +1,68 @@
+import networkx as nx
+
+
+"""
+This week’s question:
+Given an n x m matrix where all of the units are 0s except for an 1 for “start”, 
+a 2 for “end”, and 3s for walls, 
+find the shortest paths that you can take to get from 1 to 2, 
+while working around 3s.
+"""
+
+
+# convert an n x m matrix to list of edges
+# At i, j, there is a path from i to i+i if matrix[i+1,j] =! 3 and i+1 < n
+# At i, j, there is a path from i to j+1 if matrix[i,j+1] =! 3 and j+1 < m
+# If matrix[i,j] = 1, then i is the start node
+# If matrix[i,j] = 2, then j is the end node
+def matrix_to_edge_list(matrix):
+    start_node = None
+    end_node = None
+    n = len(matrix)
+    m = len(matrix[0])
+    edges = []
+    for i in range(n):
+        for j in range(m):
+            node_name = f"{i},{j}"
+            # start node?
+            if matrix[i][j] == 1:
+                start_node = node_name
+            # end node?
+            if matrix[i][j] == 2:
+                end_node = node_name
+            if i + 1 < n and matrix[i + 1][j] != 3:
+                edges.append((node_name, f"{i+1},{j}", "right"))
+                # edges.append((i, i + i))
+            if j + 1 < m and matrix[i][j + 1] != 3:
+                edges.append((node_name, f"{i},{j+1}", "down"))
+                # edges.append((i, j + 1))
+    return (start_node, end_node, edges)
+
+
+def matrix_to_graph(matrix):
+    start_node, end_node, edges = matrix_to_edge_list(matrix)
+    G = nx.DiGraph()
+    for a, b, direction in edges:
+        G.add_edge(a, b, direction=direction)
+    return (start_node, end_node, G)
+
+
+def n_grams(input, n):
+    return zip(*[input[i:] for i in range(n)])
+
+
+def path_directions(G, path):
+    return [G[u][v]["direction"] for u, v in n_grams(path, 2)]
+
+
+def start_to_end(grid):
+    start_node, end_node, G = matrix_to_graph(grid)
+    paths = nx.all_simple_paths(G, source=start_node, target=end_node)
+    return [path_directions(G, path) for path in paths]
+
+
+if __name__ == "__main__":
+    grid_1 = [[1, 0, 0], [0, 0, 2]]
+    print(list(start_to_end(grid_1)))
+    grid_2 = [[1, 3, 0], [0, 0, 2]]
+    print(list(start_to_end(grid_2)))
