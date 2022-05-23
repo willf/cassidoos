37b79ccfa1459fa2f455cd26f5701ac807cea125
Will Fitzgerald <willf@github.com>
2022-05-23 19:10:04 -0400

    add more of the example text

--- shortest-paths-2022-05-23/shortest-paths.py
+++ shortest-paths-2022-05-23/shortest-paths.py
@@ -8,6 +8,24 @@ Given an n x m matrix where all of the units are 0s except for an 1 for “start
 a 2 for “end”, and 3s for walls, 
 find the shortest paths that you can take to get from 1 to 2, 
 while working around 3s.
+
+Example (TypeScript):
+
+let grid = [
+[1,0,0],
+[0,0,2]
+]
+
+let grid2 = [
+[1,3,0],
+[0,0,2]
+]
+
+$ startToEnd(grid)
+$ [['right', 'right', 'down'], ['right', 'down', 'right'], ['down', 'right', 'right']]
+
+$ startToEnd(grid2)
+$ [['down', 'right', 'right']]
 """
 
 
