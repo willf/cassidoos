460d0c0df35d7d99c638ff380b6f468088a21fad
Will Fitzgerald <willf@github.com>
2022-07-06 14:48:45 -0400

    chuck a luck

--- chuck-a-luck/chuck-a-luck.py
+++ chuck-a-luck/chuck-a-luck.py
@@ -71,3 +71,498 @@ if __name__ == "__main__":
     print("You matched {} dice!".format(game.last_matches))
   print("You're out of money! Thanks for playing!")
   print("You played {} turns!".format(game.turns))
+
+
+""" Example game output:
+
+Your balance is: 500
+What do you want to bet?
+250
+Choose a number between 1 and 6:
+3
+Your balance is: 250
+Your last roll was: (6, 4, 2)
+You lost 250!
+What do you want to bet?
+125
+Choose a number between 1 and 6:
+3
+Your balance is: 375
+Your last roll was: (3, 4, 5)
+You matched 1 dice!
+You won 125!
+What do you want to bet?
+200
+Choose a number between 1 and 6:
+3
+Your balance is: 175
+Your last roll was: (6, 2, 6)
+You lost 200!
+What do you want to bet?
+100
+Choose a number between 1 and 6:
+2
+Your balance is: 275
+Your last roll was: (6, 1, 2)
+You matched 1 dice!
+You won 100!
+What do you want to bet?
+150
+Choose a number between 1 and 6:
+2
+Your balance is: 125
+Your last roll was: (1, 4, 6)
+You lost 150!
+What do you want to bet?
+65
+Choose a number between 1 and 6:
+3
+Your balance is: 60
+Your last roll was: (1, 4, 4)
+You lost 65!
+What do you want to bet?
+30
+Choose a number between 1 and 6:
+2
+Your balance is: 90
+Your last roll was: (5, 2, 1)
+You matched 1 dice!
+You won 30!
+What do you want to bet?
+45
+Choose a number between 1 and 6:
+2
+Your balance is: 45
+Your last roll was: (5, 5, 5)
+You lost 45!
+What do you want to bet?
+25
+Choose a number between 1 and 6:
+2
+Your balance is: 20
+Your last roll was: (6, 3, 1)
+You lost 25!
+What do you want to bet?
+10
+Choose a number between 1 and 6:
+2
+Your balance is: 30
+Your last roll was: (1, 4, 2)
+You matched 1 dice!
+You won 10!
+What do you want to bet?
+15
+Choose a number between 1 and 6:
+2
+Your balance is: 15
+Your last roll was: (1, 1, 3)
+You lost 15!
+What do you want to bet?
+7
+Choose a number between 1 and 6:
+2
+Your balance is: 8
+Your last roll was: (3, 6, 3)
+You lost 7!
+What do you want to bet?
+4
+Choose a number between 1 and 6:
+2
+Your balance is: 12
+Your last roll was: (5, 6, 2)
+You matched 1 dice!
+You won 4!
+What do you want to bet?
+6
+Choose a number between 1 and 6:
+2
+Your balance is: 18
+Your last roll was: (4, 4, 2)
+You matched 1 dice!
+You won 6!
+What do you want to bet?
+9
+Choose a number between 1 and 6:
+3
+Your balance is: 27
+Your last roll was: (1, 3, 5)
+You matched 1 dice!
+You won 9!
+What do you want to bet?
+18
+Choose a number between 1 and 6:
+3
+Your balance is: 45
+Your last roll was: (3, 6, 2)
+You matched 1 dice!
+You won 18!
+What do you want to bet?
+22
+Choose a number between 1 and 6:
+4
+Your balance is: 23
+Your last roll was: (5, 2, 3)
+You lost 22!
+What do you want to bet?
+11
+Choose a number between 1 and 6:
+2
+Your balance is: 34
+Your last roll was: (2, 1, 1)
+You matched 1 dice!
+You won 11!
+What do you want to bet?
+17
+Choose a number between 1 and 6:
+2
+Your balance is: 17
+Your last roll was: (5, 3, 4)
+You lost 17!
+What do you want to bet?
+6
+Choose a number between 1 and 6:
+3
+Your balance is: 11
+Your last roll was: (1, 2, 5)
+You lost 6!
+What do you want to bet?
+5
+Choose a number between 1 and 6:
+2
+Your balance is: 16
+Your last roll was: (3, 2, 4)
+You matched 1 dice!
+You won 5!
+What do you want to bet?
+8
+Choose a number between 1 and 6:
+2
+Your balance is: 8
+Your last roll was: (4, 4, 6)
+You lost 8!
+What do you want to bet?
+4
+Choose a number between 1 and 6:
+2
+Your balance is: 4
+Your last roll was: (3, 4, 5)
+You lost 4!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+2
+Your balance is: 6
+Your last roll was: (5, 6, 2)
+You matched 1 dice!
+You won 2!
+What do you want to bet?
+3
+Choose a number between 1 and 6:
+2
+Your balance is: 3
+Your last roll was: (5, 4, 1)
+You lost 3!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+2
+Your balance is: 4
+Your last roll was: (6, 5, 2)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+2
+Your balance is: 8
+Your last roll was: (2, 2, 1)
+You matched 2 dice!
+You won 4!
+What do you want to bet?
+4
+Choose a number between 1 and 6:
+4
+Your balance is: 12
+Your last roll was: (6, 6, 4)
+You matched 1 dice!
+You won 4!
+What do you want to bet?
+6
+Choose a number between 1 and 6:
+3 
+Your balance is: 18
+Your last roll was: (3, 4, 4)
+You matched 1 dice!
+You won 6!
+What do you want to bet?
+9
+Choose a number between 1 and 6:
+2
+Your balance is: 36
+Your last roll was: (2, 3, 2)
+You matched 2 dice!
+You won 18!
+What do you want to bet?
+18
+Choose a number between 1 and 6:
+3
+Your balance is: 18
+Your last roll was: (2, 6, 5)
+You lost 18!
+What do you want to bet?
+9
+Choose a number between 1 and 6:
+3
+Your balance is: 27
+Your last roll was: (5, 3, 4)
+You matched 1 dice!
+You won 9!
+What do you want to bet?
+15
+Choose a number between 1 and 6:
+3
+Your balance is: 42
+Your last roll was: (1, 3, 6)
+You matched 1 dice!
+You won 15!
+What do you want to bet?
+22
+Choose a number between 1 and 6:
+3
+Your balance is: 86
+Your last roll was: (3, 5, 3)
+You matched 2 dice!
+You won 44!
+What do you want to bet?
+43
+Choose a number between 1 and 6:
+3
+Your balance is: 43
+Your last roll was: (4, 6, 4)
+You lost 43!
+What do you want to bet?
+21
+Choose a number between 1 and 6:
+3
+Your balance is: 22
+Your last roll was: (6, 1, 6)
+You lost 21!
+What do you want to bet?
+11
+Choose a number between 1 and 6:
+3
+Your balance is: 11
+Your last roll was: (4, 6, 4)
+You lost 11!
+What do you want to bet?
+5
+Choose a number between 1 and 6:
+3
+Your balance is: 16
+Your last roll was: (1, 4, 3)
+You matched 1 dice!
+You won 5!
+What do you want to bet?
+8
+Choose a number between 1 and 6:
+3
+Your balance is: 24
+Your last roll was: (3, 6, 6)
+You matched 1 dice!
+You won 8!
+What do you want to bet?
+12
+Choose a number between 1 and 6:
+3
+Your balance is: 36
+Your last roll was: (5, 4, 3)
+You matched 1 dice!
+You won 12!
+What do you want to bet?
+18
+Choose a number between 1 and 6:
+3
+Your balance is: 18
+Your last roll was: (2, 6, 6)
+You lost 18!
+What do you want to bet?
+9
+Choose a number between 1 and 6:
+3
+Your balance is: 9
+Your last roll was: (6, 4, 5)
+You lost 9!
+What do you want to bet?
+4
+Choose a number between 1 and 6:
+3
+Your balance is: 5
+Your last roll was: (6, 4, 5)
+You lost 4!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+3
+Your balance is: 3
+Your last roll was: (4, 1, 1)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+3
+Your balance is: 2
+Your last roll was: (4, 5, 1)
+You lost 1!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 3
+Your last roll was: (1, 4, 6)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 4
+Your last roll was: (6, 1, 6)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+2
+Your balance is: 2
+Your last roll was: (5, 3, 4)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 1
+Your last roll was: (3, 6, 6)
+You lost 1!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+3
+Your balance is: 3
+Your last roll was: (6, 3, 3)
+You matched 2 dice!
+You won 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+2
+Your balance is: 5
+Your last roll was: (2, 6, 2)
+You matched 2 dice!
+You won 2!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+3
+Your balance is: 3
+Your last roll was: (5, 2, 1)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 4
+Your last roll was: (1, 4, 3)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+1
+Your balance is: 2
+Your last roll was: (4, 4, 6)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+2
+Your balance is: 3
+Your last roll was: (5, 4, 2)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 4
+Your last roll was: (1, 5, 4)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+1
+Your balance is: 2
+Your last roll was: (5, 3, 6)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 5
+Your last roll was: (1, 1, 1)
+You matched 3 dice!
+You won 3!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+3
+Your balance is: 3
+Your last roll was: (2, 2, 1)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 4
+Your last roll was: (4, 3, 1)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+2
+Your balance is: 2
+Your last roll was: (5, 1, 3)
+You lost 2!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 3
+Your last roll was: (1, 2, 6)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+1
+Choose a number between 1 and 6:
+1
+Your balance is: 4
+Your last roll was: (4, 3, 1)
+You matched 1 dice!
+You won 1!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+2
+Your balance is: 2
+Your last roll was: (5, 3, 3)
+You lost 2!
+What do you want to bet?
+2
+Choose a number between 1 and 6:
+2
+Your balance is: 0
+Your last roll was: (4, 3, 5)
+You're out of money! Thanks for playing!
+You played 65 turns!
+"""
